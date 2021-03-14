fd1caeccbcda01d41423901b20ff9551e6ab1057
rod martin <rod@araknia.com>
2021-03-14 15:45:55 +0000

    Camera Follow Code tidy

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+ 
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position+offset, 0.1f);
+        }
+    }
+}
--- Assets/Scripts/ChipController.cs
+++ Assets/Scripts/ChipController.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+
 [RequireComponent(typeof(Rigidbody2D))]
 public class ChipController : MonoBehaviour
 {
+    public enum ChipStates {SCAN, CLEAN}
+    public ChipStates chipstate = ChipStates.SCAN;
     private Rigidbody2D rb;
     // use the turn speed for its rotation scanning
     public float turnSpeed = 30f;
@@ -11,19 +16,53 @@ public class ChipController : MonoBehaviour
     // how fast can it move
     public float moveSpeed = 5f;
     public Transform rayFrom;    // detect ray from here
-    public LayerMask rayCastOn;  // use this layer stop stop ray testing yourself!
+    public LayerMask rayCastOn;  // raycast on this layer!
+    public Grid grid;
+    public Tilemap tilemap;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        switch (chipstate)
+        {
+            case ChipStates.SCAN:
+                Scan();
+                break;
+            case ChipStates.CLEAN:
+                Clean();
+                break;
+        }
+
+        
+    }
+
+    void Clean()
+    {
+        rb.AddForce(transform.up * moveSpeed);
+     
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // is it a virus?
+        if (other.CompareTag("virus"))
+        {
+            // find where on the grid this is
+            Vector3Int position = grid.WorldToCell(rayFrom.position); 
+            tilemap.SetTile(position, null); // set tile at this position as null
+            rb.velocity = Vector2.zero; // stop any movement
+            chipstate = ChipStates.SCAN; // resume scan
+        }
+    }
+ 
+    void Scan()
     {
         rb.angularVelocity = turnSpeed;
-        Vector3 forward = transform.TransformDirection(Vector3.up) * rayDistance; 
-        
-      
+        Vector3 forward = transform.TransformDirection(Vector3.up) * rayDistance;
         // test for any hits
         RaycastHit2D hit = Physics2D.Raycast( rayFrom.position, forward, rayDistance, rayCastOn);
         if (hit.collider != null)
@@ -31,10 +70,11 @@ public class ChipController : MonoBehaviour
             Debug.Log($"Hit {hit.collider.name}");
             // draw red if hit
             Debug.DrawRay( rayFrom.position, forward, Color.red);
+            chipstate = ChipStates.CLEAN;
         }
         else
         {
-           // draws green if no hit
+            // draws green if no hit
             Debug.DrawRay( rayFrom.position, forward, Color.green);
         }
     }

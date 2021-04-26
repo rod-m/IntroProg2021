c99205206a2f6214af1954f133c166b567d77917
rod martin <rod@araknia.com>
2021-04-26 10:48:53 +0100

    setup for door open system

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,6 +7,7 @@ public class GameController : MonoBehaviour
     public static GameController Instance;
     public HUDController hudController;
     private int playerScore = 0;
+    private int keysCollected = 0;
     void Start()
     {
         if (Instance != null && Instance != this)
@@ -27,5 +28,11 @@ public class GameController : MonoBehaviour
         Debug.Log("Score added!");
 
         hudController.SetScore("Score " + playerScore);
+    }
+
+    public void AddKey()
+    {
+        keysCollected++;
+        Debug.Log("Keys collected " + keysCollected);
     }
 }
